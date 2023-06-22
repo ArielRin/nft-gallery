73989b6edb155dcc07946b79fc0e0b1b8bc757c2
ArielRin <girlsmakingcrypto@gmail.com>
2023-06-22 16:20:54 +1000

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,12 +2,12 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
+export const contractAddress = "0x1b7C6855d2f65ec78828fCa07cC9170088E1815c";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
 // Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
-export const chain = "ethereum";
+export const chain = "BinanceSmartChainMainnet ";
 
 // The block explorer you want to use (Opens when user clicks on history of events. i.e. transfers)
-export const blockExplorer = "https://etherscan.io";
+export const blockExplorer = "https://bsc-dataseed1.ninicoin.io";
